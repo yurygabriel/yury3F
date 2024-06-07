b290412dc801c1c85760a3a2a8e735e884127065
yurygabriel <110116044+yurygabriel@users.noreply.github.com>
2024-06-07 08:22:21 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const botoes = document.querySelectorAll(".botao");
-
+const conteudo = document.querySelectorAll(".aba-conteudo")
 
 for (let i = 0 ; i < botoes.length ; i++){
     botoes[i].onclick = function (){
         for (let j = 0; j < botoes.length ; j++){
             botoes[j].classList.remove("ativo")
+            conteudo[j].classList.remove("ativo")
         }
         botoes[i].classList.add("ativo")
+        conteudo[i].classList.add("ativo")
     }
 }
