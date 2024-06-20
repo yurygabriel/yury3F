dbc4496a5da8a74ffdd5870a75cc129439cb4a88
yurygabriel <110116044+yurygabriel@users.noreply.github.com>
2024-06-20 09:45:45 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo")
 const contador = document.querySelectorAll(".contador")
-contador[0].textContent = "14/06/2024"
-const tempoObjetivo1 = new Date (2024,8,2)
-const tempoObjetivo2 = new Date (2024,7,10)
-const tempoObjetivo3 = new Date (2024,10,11)
-const tempoObjetivo4 = new Date (2024,11,12)
+contador[0].textContent = "ola"
+const  tempoObjetivo1 = new Date()
+const  tempoObjetivo2 = new Date()
+const  tempoObjetivo3 = new Date()
+const  tempoObjetivo4 = new Date()
+let agora = new Date ();
+    contador[0].textContent = tempoObjetivo1-agora
+    contador[1].textContent = tempoObjetivo2-agora
+    contador[2].textContent = tempoObjetivo3-agora
+    contador[3].textContent = tempoObjetivo4-agora
+
 for (let i = 0 ; i < botoes.length ; i++){
     botoes[i].onclick = function (){
         for (let j = 0; j < botoes.length ; j++){
