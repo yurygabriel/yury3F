65b4225af59930a421a7fbd18c22d75d4b66c22f
yurygabriel <110116044+yurygabriel@users.noreply.github.com>
2024-06-27 09:13:43 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,39 @@
+
 const botoes = document.querySelectorAll(".botao");
-const conteudo = document.querySelectorAll(".aba-conteudo")
-const contador = document.querySelectorAll(".contador")
-contador[0].textContent = "ola"
-const  tempoObjetivo1 = new Date()
-const  tempoObjetivo2 = new Date()
-const  tempoObjetivo3 = new Date()
-const  tempoObjetivo4 = new Date()
-let agora = new Date ();
-    contador[0].textContent = tempoObjetivo1-agora
-    contador[1].textContent = tempoObjetivo2-agora
-    contador[2].textContent = tempoObjetivo3-agora
-    contador[3].textContent = tempoObjetivo4-agora
+const conteudo = document.querySelectorAll(".aba-conteudo");
+const contador = document.querySelectorAll(".contador");
+
+contador[0].textContent = "Olá"
+const tempoObjetivo1 = new Date(2024,11,31,23,59)
+const tempoObjetivo2 = new Date(2024,11,31,23,59)
+const tempoObjetivo3 = new Date(2024,11,31,23,59)
+const tempoObjetivo4 = new Date(2024,11,31,23,59)
+
+let agora = new Date();
+contador[0].textContent = tempoObjetivo1-agora
+contador[1].textContent = tempoObjetivo2-agora
+contador[2].textContent = tempoObjetivo3-agora
+contador[3].textContent = tempoObjetivo4-agora
+
+let segundos
+let minutos
+let horas
+let dia
+
+segundos = (tempoObjetivo1-agora)/1000
+minutos = segundos/60
+horas = minutos/60
+dias = horas/24
+
+contador[0].textContent = `Faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos`
 
 for (let i = 0 ; i < botoes.length ; i++){
     botoes[i].onclick = function (){
         for (let j = 0; j < botoes.length ; j++){
-            botoes[j].classList.remove("ativo")
-            conteudo[j].classList.remove("ativo")
+        botoes[j].classList.remove("ativo");
+        conteudo[j].classList.remove('ativo')
         }
-        botoes[i].classList.add("ativo")
+        botoes[i].classList.add("ativo");
         conteudo[i].classList.add("ativo")
     }
 }
