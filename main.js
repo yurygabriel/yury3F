f2a599c063406046e4f67cddc7379f59afa6ffa5
yurygabriel <110116044+yurygabriel@users.noreply.github.com>
2024-07-25 09:43:33 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,63 +1,47 @@
 
 const botoes = document.querySelectorAll(".botao");
-const conteudo = document.querySelectorAll(".aba-conteudo");
-const contador = document.querySelectorAll(".contador");
-
-contador[0].textContent = "Olá"
-const tempoObjetivo1 = new Date(2024,06,27,09,19)
-const tempoObjetivo2 = new Date(2024,06,27,09,19)
-const tempoObjetivo3 = new Date(2024,06,27,09,19)
-const tempoObjetivo4 = new Date(2024,07,27,09,19)
+const conteudo = document.querySelectorAll(".aba-conteudo")
+const contador = document.querySelectorAll(".contador")
+const tempoObjetivo1 = new Date(2025,10,30,20)
+const tempoObjetivo2 = new Date(2025,1,30,20)
+const tempoObjetivo3 = new Date(2025,3,30,20)
+const tempoObjetivo4 = new Date(2025,4,30,20)
 
 function calculaTempo(tempoObjetivo){
-const agora = new Date()
-let minutos
-let segundos
-let horas
-let dias
-segundos = (tempoObjetivo - agora)/1000
-minutos = segundos/60;
-horas = minutos/60;
-dias = horas/24;
-segundos = Math.floor(segundos);
-minutos = Math.floor(minutos);
-horas = Math.floor (horas);
-dias = Math.floor (dias);
+    const agora = new Date()
+    let segundos
+    let minutos
+    let horas
+    let dias
+    segundos = (tempoObjetivo - agora)/1000
+    minutos = segundos/60
+    horas = minutos/60
+    dias = horas/24
+    segundos = Math.floor(segundos)
+    minutos = Math.floor(minutos)
+    horas = Math.floor(horas)
+    dias = Math.floor(dias)
+
+    segundos = segundos%60
+    minutos = minutos%60
+    horas = horas%24
+    return [dias,horas,minutos,segundos]
 }
-
-
-let agora = new Date();
-contador[0].textContent = tempoObjetivo1-agora
-contador[1].textContent = tempoObjetivo2-agora
-contador[2].textContent = tempoObjetivo3-agora
-contador[3].textContent = tempoObjetivo4-agora
-
-let segundos
-let minutos
-let horas
-let dias
-
-segundos = (tempoObjetivo1-agora)/1000
-minutos = segundos/60;
-horas = minutos/60;
-dias = horas/24;
-segundos = Math.floor(segundos);
-minutos = Math.floor(minutos);
-horas = Math.floor (horas);
-dias = Math.floor (dias);
-segundos = segundos%60;
-minutos = minutos%60;
-horas = horas%24;
-
-contador[0].textContent = `Faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos`
-
+let contadorPrimeiro = calculaTempo(tempoObjetivo1)
+contador[0].textContent = `Faltam ${contadorPrimeiro[0]} dias, ${contadorPrimeiro[1]} horas, ${contadorPrimeiro[2]} minutos e ${contadorPrimeiro[3]} segundos`
+let contadorSegundo = calculaTempo(tempoObjetivo2)
+contador[1].textContent = `Faltam ${contadorSegundo[0]} dias, ${contadorSegundo[1]} horas e ${contadorSegundo[2]} minutos e ${contadorSegundo[3]} segundos`
+let contadorTerceiro = calculaTempo(tempoObjetivo2)
+contador[2].textContent = `Faltam ${contadorTerceiro[0]} dias, ${contadorTerceiro[1]} horas e ${contadorTerceiro[2]} minutos e ${contadorTerceiro[3]} segundos`
+let contadorQuarto = calculaTempo(tempoObjetivo2)
+contador[3].textContent = `Faltam ${contadorQuarto[0]} dias, ${contadorQuarto[1]} horas e ${contadorQuarto[2]} minutos e ${contadorQuarto[3]} segundos`
 for (let i = 0 ; i < botoes.length ; i++){
     botoes[i].onclick = function (){
         for (let j = 0; j < botoes.length ; j++){
-        botoes[j].classList.remove("ativo");
-        conteudo[j].classList.remove('ativo')
+            botoes[j].classList.remove("ativo")
+            conteudo[j].classList.remove("ativo")
         }
-        botoes[i].classList.add("ativo");
+        botoes[i].classList.add("ativo")
         conteudo[i].classList.add("ativo")
     }
 }
